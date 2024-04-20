8d40ea7c6515505829758b7113ae953f4be969ce
NameOfFool <ivan.pauk2008@gmail.com>
2024-04-20 14:47:51 +0300

    enemy pathfinding

---
+++ Assets/Scripts/Enemy/AIVision.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class AIVision : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    private NavMeshAgent _agent;
+    void Awake()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _agent.updateRotation = false;
+        _agent.updateUpAxis = false;
+    }
+
+    void Update()
+    {
+        _agent.SetDestination(target.position);
+    }
+}
